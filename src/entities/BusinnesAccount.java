2289b853a5c6751792a318198f06564633f90960
rodrigodojo <rodrigodoj@gmail.com>
2021-01-26 10:03:29 -0300

    Exemplo de herança

---
+++ src/entities/BusinnesAccount.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BusinnesAccount extends Account{
+
+	private Double loanLimit;
+	
+	public BusinnesAccount() {
+		super();
+	}
+
+	public BusinnesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= loanLimit) {
+			balance += amount - 10;
+		}	
+	}
+}
